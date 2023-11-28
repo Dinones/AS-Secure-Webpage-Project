e03fad7e7e871a8c34dc8395c57ecbc77131c1d7
Dinones <leixpinyon6@gmail.com>
2023-11-28 18:09:56 +0100

    Merged changes

--- server.py
+++ server.py
@@ -1,17 +1,31 @@
-from flask import Flask, render_template, request, redirect, url_for, send_file
+from flask import Flask, render_template, request, redirect, url_for, session, send_file
 from check_pdf import is_valid_pdf, save_pdf
 from enum import Enum
 import pyodbc
 import bcrypt
 import os
+import uuid
+import secrets
 
 #################################################################################################################
 
 app = Flask(__name__, template_folder='templates', static_folder='static')
+app.config['SECRET_KEY'] = secrets.token_hex(16)  # Generates a 32-character hex secret key
+
 connection = None
 cursor = None
 
-class User(Enum):
+# Dictionary to store active sessions (token, user)
+active_sessions = {}
+
+class UserInformation:
+    def __init__(self, userMail, userID, userType):
+        self.userMail = userMail
+        self.userID = userID
+        self.userType = userType
+
+
+class UserType(Enum):
     APPLICANT = 1
     RECRUITER = 2
 
@@ -27,11 +41,31 @@ def about():
 
 @app.route('/applicant')
 def applicant():
-    return render_template('applicant.html')
+    if 'session_token' in session:
+        session_token = session['session_token']
+
+        if session_token in active_sessions:
+            ActiveUser = active_sessions[session_token]
+            print(f"Active user's mail is {ActiveUser.userMail}, with ID = {ActiveUser.userID}")
+        else: return 'ERROR: Invalid session.'
+    else: return 'ERROR: Not logged in.'
+
+    if ActiveUser.userType == UserType.APPLICANT: return render_template('applicant.html')
+    else: return "ERROR: Logged user is not an applicant"
 
 @app.route('/recruiter')
 def recruiter():
-    return render_template('recruiter.html')
+    if 'session_token' in session:
+        session_token = session['session_token']
+
+        if session_token in active_sessions:
+            ActiveUser = active_sessions[session_token]
+            print(f"Active user's mail is {ActiveUser.userMail}, with ID = {ActiveUser.userID}")
+        else: return 'ERROR: Invalid session.'
+    else: return 'ERROR: Not logged in.'
+
+    if ActiveUser.userType == UserType.RECRUITER: return render_template('recruiter.html')
+    else: return "ERROR: Logged user is not a recruiter"
 
 @app.route('/say_hello', methods=['POST'])
 def say_hello():
@@ -44,25 +78,40 @@ def check_credentials():
     hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
     print(f"Username: {username}, Unhashed-Password: {password}, Hashed-Password: {hashed_password}")
-    UserType = check_login(username, password)
+    userInfo = check_login(username, password)
     print(f'UserType: {UserType}')
 
-    if UserType == 0: return redirect(url_for('home'))
-    elif UserType == 1: return redirect(url_for('applicant'))
-    else: return redirect(url_for('recruiter'))
+    if userInfo.userType == 0: return redirect(url_for('home'))
+    else:
+        session_token = str(uuid.uuid4())
+        active_sessions[session_token] = userInfo
+        session['session_token'] = session_token
+
+        if userInfo.userType == UserType.APPLICANT: return redirect(url_for('applicant'))
+        else: return redirect(url_for('recruiter'))
+
 
 @app.route('/upload', methods=['POST']) 
 def upload():
+    if 'session_token' in session:
+        session_token = session['session_token']
+
+        if session_token in active_sessions:
+            ActiveUser = active_sessions[session_token]
+            print(f"Active user's mail is {ActiveUser.userMail}, with ID = {ActiveUser.userID}")
+        else: return 'ERROR: Invalid session.'
+    else: return 'ERROR: Not logged in.'
+
     if 'file' in request.files:
         uploaded_file = request.files['file']
 
         # Check if the file has a valid PDF extension
         if uploaded_file and uploaded_file.filename.endswith('.pdf'):
-            uploaded_file.save(f"temp_files/temp_CV.pdf")
-            if not is_valid_pdf(f"temp_files/temp_CV.pdf"): 
-                os.remove(f"temp_files/temp_CV.pdf")
+            uploaded_file.save(f"./temp_files/temp_CV.pdf")
+            if not is_valid_pdf(f"./temp_files/temp_CV.pdf"): 
+                os.remove(f"./temp_files/temp_CV.pdf")
                 return 'ERROR: Invalid file format. Please upload a PDF.'
-            upload_pdf_to_database()
+            upload_pdf_to_database(f"./temp_files/temp_CV.pdf", ActiveUser.userID)
 
             print(f"\nUploaded file saved: {uploaded_file.filename}\n")
             return 'File uploaded successfully!'
@@ -106,29 +155,33 @@ def check_login(email, password):
     rows = cursor.fetchall()
     if len(rows) != 1: 
         print('ERROR: Command did not return a single row!')
-        return 0
+        return UserInformation("", 0, 0)
     
     if bcrypt.checkpw(password.encode('utf-8'), rows[0][1].encode('utf-8')):
-        return User.RECRUITER.value if rows[0][2] == 'Recruiter' else User.APPLICANT.value
-    else: return 0
+        userInfo = UserInformation(email, rows[0][0], 0)
+        if rows[0][2] == 'Recruiter': userInfo.userType = UserType.RECRUITER 
+        else: userInfo.userType = UserType.APPLICANT
+        return userInfo 
+    else: 
+        print('ERROR: Password did not match!')
+        return UserInformation("", 0, 0)
 
-def upload_pdf_to_database(pdf_path = './temp_files/temp_CV.pdf'):
+def upload_pdf_to_database(pdf_path, userID):
     global cursor, connection
 
-    if not os.path.exists(pdf_path): return print(f"Error uploading file to the database")
     try:
         with open(pdf_path, 'rb') as file:
-            cursor.execute(f"UPDATE Applicant SET [CV] = (?) WHERE UserID = 1", file.read())
+            cursor.execute(f"UPDATE Applicant SET [CV] = (?) WHERE UserID = {userID}", file.read())
         connection.commit()
         print("File successfully updated!")
     except pyodbc.Error as e: 
         print(f"Error uploading file to the database: {e}")
     os.remove(pdf_path)
 
-def download_pdf_from_database(output_pdf_path = './temp_files/temp_CV.pdf'):
+def download_pdf_from_database(userID, output_pdf_path = './temp_files/temp_CV.pdf'):
     global cursor, connection
 
-    cursor.execute(f"SELECT [CV] FROM Applicant WHERE UserID = 1")
+    cursor.execute(f"SELECT [CV] FROM Applicant WHERE UserID = {userID}")
     row = cursor.fetchone()
     if row:
         with open(output_pdf_path, 'wb') as file:
@@ -136,7 +189,7 @@ def download_pdf_from_database(output_pdf_path = './temp_files/temp_CV.pdf'):
 
 @app.route('/send_document_to_user')
 def send_document_to_user():
-    download_pdf_from_database()
+    download_pdf_from_database(2)
     document_path = './temp_files/temp_CV.pdf'
     document_name = 'CV.pdf'
 
